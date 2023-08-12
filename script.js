45c8e426172d0dde10d8a00a2d491ec4bd3e2c8b
coutbiaafnts <bia.fontes653@gmail.com>
2023-08-12 19:50:27 -0300

    :tadda: initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+function toggleMode() {
+	const html = document.documentElement
+
+	// toggle == alterna
+	html.classList.toggle("light")
+
+	// condicional
+	// const img = document.querySelector("#profile img")
+	// troca a imagem
+	// if (html.classList.contains("light")) {
+	// 	  img.setAttribute("src", "")
+	// } else {
+	// 	  img.setAttribute("src", "")
+	// }
+}
